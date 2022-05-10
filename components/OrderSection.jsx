20b6dd8dc147264107d560b64e070a637997c740
Takara Truong <truont2@gmail.com>
2022-05-10 11:58:53 -0700

    saving

--- components/OrderSection.jsx
+++ components/OrderSection.jsx
@@ -12,24 +12,28 @@ export const OrderSection = () => {
       title: "GrubHub",
       width: "80%",
       image: "/assets/grubhublogo.png",
+      description: 'Delivery Service'
     },
     {
       url: "https://www.doordash.com/store/fat-pig-bbq-edmonds-429805/?utm_campaign=gpa",
       title: "DoorDash",
       width: "80%",
       image: "/assets/doordashlogo.png",
+      description: 'Delivery Service'
     },
     {
       url: "https://www.ubereats.com/store/fat-pig-bbq/S0VhUzV1RnyR1Jh3vmWdHw",
       title: "Uber Eats",
       width: "80%",
       image: "/assets/ubereatslogo.png",
+      description: 'Delivery Service'
     },
     {
       url: "https://www.google.com/",
       title: "SquareSpace",
       width: "80%",
       image: "/assets/ubereatslogo.png",
+      description: 'Delivery Service'
     },
   ];
 
@@ -45,11 +49,12 @@ export const OrderSection = () => {
   justifyContent="center"
         >
           {images.map((image) => (
-            <Grid item xs={12} s={6} md={4} lg={2} className='ordercards'>
-              <a href={image.url} target={"_blank"} rel={"noreferrer"}>
-                <OrderCard image={image} />
-              </a>
-            </Grid>
+            <Grid item 
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center">
+              <OrderCard image={image} />
+          </Grid>
           ))}
         </Grid>
       </Box>
