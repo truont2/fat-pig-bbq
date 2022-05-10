88eed6629fc83cb26b0b716b25f7efcb5739a16b
Andrew Tran <andrewtranmsw@gmail.com>
2022-05-10 11:57:05 -0700

    login and signup done. need cards

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import styles from '../styles/Login.module.css';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import styles from "../styles/Login.module.css";
+import Card from "react-bootstrap/Card";
+import Link from "@mui/material/Link";
+import { Container } from "@mui/material";
 
 export default function login() {
   return (
-    <div className={styles.content}>
-        <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 1,
-          width: 128,
-          height: 128,
-        },
-      }}
-    >
-      <Paper elevation={0} />
-      <Paper />
-      <Paper elevation={3} />
-    </Box>
-    </div>
-  )
+    <Card className={styles.content}>
+      <form>
+        <Card.Title>Sign In</Card.Title>
+        <div>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+        <h6>
+          <a href="#">Forgot password?</a>
+        </h6>
+
+        <h6>
+          Don't have an account? <Link href="/signup">Sign Up</Link>
+        </h6>
+      </form>
+    </Card>
+  );
 }
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import styles from "../styles/Login.module.css";
+import Card from "react-bootstrap/Card";
 
 export default function signup() {
   return (
-    <div>signup</div>
-  )
+    <Card className={styles.content}>
+      <form>
+        <Card.Title>Sign Up</Card.Title>
+        <div>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+        <h6>
+          Already have an account? <Link href="/login">Log In</Link>
+        </h6>
+      </form>
+    </Card>
+  );
 }
