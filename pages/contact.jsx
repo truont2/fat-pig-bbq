ac642ca7762d933c3f226235a3a1a3b4555a0693
Andrew Tran <andrewtranmsw@gmail.com>
2022-05-10 14:10:07 -0700

    contact us

---
+++ pages/contact.jsx
@@ -0,0 +1,60 @@
+import "../styles/Contact.module.css";
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+
+export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_z6vzmuw",
+        "template_i07v4v9",
+        form.current,
+        "uWh6Oxid8hahrWfGQ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h2>Contact Us</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>fatpigbbq@gmail.com</h5>
+            <a href="mailto:fatpigbbq@gmail.com">Send a message</a>
+          </article>
+        </div>
+        {/* End of contact options */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
